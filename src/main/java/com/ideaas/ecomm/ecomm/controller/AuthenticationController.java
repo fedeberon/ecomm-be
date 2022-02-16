9590d7963f04ee0aaae32a7897c6fa32f2e060cb
Nicolas Basualdo Rey <95653230+NicobRey@users.noreply.github.com>
2022-02-16 14:56:49 -0300

    Avances password

--- src/main/java/com/ideaas/ecomm/ecomm/controller/AuthenticationController.java
+++ src/main/java/com/ideaas/ecomm/ecomm/controller/AuthenticationController.java
@@ -7,14 +7,20 @@ import com.ideaas.ecomm.ecomm.payload.AuthenticationResponse;
 import com.ideaas.ecomm.ecomm.services.interfaces.IUserService;
 import com.ideaas.ecomm.ecomm.services.interfaces.JwtService;
 import org.apache.catalina.connector.Response;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.StringUtils; 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+ 
 
 @RestController
 @CrossOrigin
@@ -22,28 +28,46 @@ public class AuthenticationController {
 
     private IUserService userService;
     private JwtService jwtService;
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     public AuthenticationController(final IUserService userService,
-                                    final JwtService jwtService) {
+                                    final JwtService jwtService,
+                                    final AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
-        try {
-            UserDetails userDetails = userService.loadUserByUsername(authenticationRequest.getUsername());
-            String token = jwtService.createToken(userDetails);
+    public ResponseEntity<AuthenticationResponse>createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        
+        try{
+            authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+                        
+            final UserDetails userDetails = userService.loadUserByUsername(authenticationRequest.getUsername());
+            final String token = jwtService.createToken(userDetails);
             User user = userService.get(userDetails.getUsername());
             AuthenticationResponse tokenResponse = new AuthenticationResponse(token,
                                                                               user.getUsername(),
                                                                               user.getName(),
                                                                               StringUtils.join(user.getAuthorities(), ","));
-
             return ResponseEntity.ok().body(tokenResponse);
-        } catch (InvalidPasswordOrUsernameException e) {
-            return ResponseEntity.status(Response.SC_UNAUTHORIZED).body(null);
-        }
+            
+            } catch (DisabledException e) {
+                System.out.println(e);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            } catch (BadCredentialsException e) {
+                System.out.println(e); 
+                return ResponseEntity.status(HttpStatus.GONE).build();
+            }
     }
+
+    private void authenticate(String username, String password) throws DisabledException {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (Exception e) {
+            throw new DisabledException("USER_DISABLED", e);
+        }   
+    }      
 }
