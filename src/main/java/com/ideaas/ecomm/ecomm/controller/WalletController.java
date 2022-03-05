c2a0075ef11229bd820d70ebf99bd76941232212
fedeberon <federicoberon@outlook.com>
2022-03-05 07:47:57 -0300

    Compras con tarjeta de puntos.

---
+++ src/main/java/com/ideaas/ecomm/ecomm/controller/WalletController.java
@@ -0,0 +1,50 @@
+package com.ideaas.ecomm.ecomm.controller;
+
+import com.ideaas.ecomm.ecomm.domain.Checkout;
+import com.ideaas.ecomm.ecomm.domain.User;
+import com.ideaas.ecomm.ecomm.enums.WalletTransactionType;
+import com.ideaas.ecomm.ecomm.payload.WalletDiscountRequest;
+import com.ideaas.ecomm.ecomm.services.interfaces.ICheckoutService;
+import com.ideaas.ecomm.ecomm.services.interfaces.IProductService;
+import com.ideaas.ecomm.ecomm.services.interfaces.IUserService;
+import com.ideaas.ecomm.ecomm.services.interfaces.IWalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallet")
+@CrossOrigin
+public class WalletController {
+
+    private ICheckoutService checkoutService;
+    private IUserService userService;
+    private IWalletService walletService;
+    private IProductService productService;
+
+    @Autowired
+    public WalletController(final ICheckoutService checkoutService,
+                            final IUserService userService,
+                            final IWalletService walletService,
+                            final IProductService productService) {
+        this.checkoutService = checkoutService;
+        this.userService = userService;
+        this.walletService = walletService;
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> buyWithPoints(final @RequestBody WalletDiscountRequest walletDiscountRequest) {
+        Checkout checkout = checkoutService.get(walletDiscountRequest.getCheckoutId());
+        User user = userService.get(walletDiscountRequest.getUsername());
+        walletService.productToCartInWallet(user, checkout.getProducts(), WalletTransactionType.SALE);
+        productService.discountAmountStock(checkout.getProducts());
+
+        return ResponseEntity.ok().build();
+    }
+
+}
