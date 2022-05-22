57ccba2726b405f5e7ea1912751440ddf2df2bde
fedeberon <federicoberon@outlook.com>
2022-05-22 10:02:20 -0300

    Paginado de productos

--- src/main/java/com/ideaas/ecomm/ecomm/repository/ProductDao.java
+++ src/main/java/com/ideaas/ecomm/ecomm/repository/ProductDao.java
@@ -3,7 +3,9 @@ package com.ideaas.ecomm.ecomm.repository;
 import com.ideaas.ecomm.ecomm.domain.Brand;
 import com.ideaas.ecomm.ecomm.domain.Category;
 import com.ideaas.ecomm.ecomm.domain.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -11,13 +13,15 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductDao extends JpaRepository<Product, Long> {
+public interface ProductDao extends PagingAndSortingRepository<Product, Long> {
 
-    List<Product> findAllByCategory(final Category category);
+    List<Product> findAllByCategory(Category category);
 
-    List<Product> findAllByNameContainingIgnoreCase(final String value);
+    List<Product> findAllByNameContainingIgnoreCase(String value);
 
     List<Product> searchAllByBrandIn(Collection<Brand> brandId);
 
     List<Product> searchAllByCategoryIn(Collection<Category> categories);
+
+    List<Product> findAllByNameContainingIgnoreCase(String value, Pageable pageable);
 }
