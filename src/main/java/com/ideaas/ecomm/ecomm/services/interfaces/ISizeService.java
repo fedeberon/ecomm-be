f64a48c5ec76c00d23a4b50edeb45ead922bf89a
leokhfm <leonardo.couar@gmail.com>
2022-06-09 15:48:02 -0300

    Size system

---
+++ src/main/java/com/ideaas/ecomm/ecomm/services/interfaces/ISizeService.java
@@ -0,0 +1,10 @@
+package com.ideaas.ecomm.ecomm.services.interfaces;
+
+import java.util.List;
+
+import com.ideaas.ecomm.ecomm.domain.Talle;
+
+public interface ISizeService {
+    List<Talle> findAll();
+    
+}
