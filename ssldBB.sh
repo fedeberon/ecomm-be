f145d437f576a367d0fbb2e1601da9152eb14598
leokhfm <leonardo.couar@gmail.com>
2022-08-05 11:00:12 -0300

    sh de certSSL

---
+++ ssldBB.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /etc/letsencrypt/live/vps-2124680-x.dattaweb.com-0003/
+
+sudo openssl pkcs12 -export -out cert.p12   -in fullchain.pem   -inkey privkey.pem   -name tomcat
+123
+123
+
