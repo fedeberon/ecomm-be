1dd11cb09f73fceb644ad1ac9944c6a9c40e0339
Tom <tomaslm_3@hotmail.co>
2022-11-08 09:56:33 -0300

    sizeMethods

--- src/main/java/com/ideaas/ecomm/ecomm/controller/SizeController.java
+++ src/main/java/com/ideaas/ecomm/ecomm/controller/SizeController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +31,29 @@ public class SizeController {
         List<Size> talles = sizeService.findAll();
         
         return ResponseEntity.ok(talles);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Size> findById(@PathVariable final Long id){
+        final Size size = sizeService.get(id);
+
+        return ResponseEntity.ok(size);
+    }
+
+    @PostMapping
+    public ResponseEntity<Size> save(@RequestBody Size size) {
+        final Size sizeSaved = sizeService.save(size);
+
+        return ResponseEntity.accepted().body(sizeSaved);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Size> update(@PathVariable final Long id, @RequestBody final Size size){
+        final Size sizeToUpdate = sizeService.get(id);
+
+        sizeToUpdate.setName(size.getName());
+
+        sizeService.save(sizeToUpdate);
+        return ResponseEntity.ok(sizeToUpdate);
     }
 }
