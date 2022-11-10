e41b2ad4f926e6b2b7816a2838d030ad05437f9d
Tom <tomaslm_3@hotmail.co>
2022-11-10 17:14:25 -0300

    addCrossOrigin-SizeController

--- src/main/java/com/ideaas/ecomm/ecomm/controller/SizeController.java
+++ src/main/java/com/ideaas/ecomm/ecomm/controller/SizeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.ideaas.ecomm.ecomm.services.interfaces.ISizeService;
 
 @RestController
 @RequestMapping("size")
+@CrossOrigin
 public class SizeController {
     
     private ISizeService sizeService;
