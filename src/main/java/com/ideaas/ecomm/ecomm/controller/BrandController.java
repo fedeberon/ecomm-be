ed7973887c8094f7c2e3a61cd836fd2f6eb2ff32
Tom <tomaslm_3@hotmail.co>
2022-12-08 17:04:41 -0300

    Minor chage - Crud delete brands

--- src/main/java/com/ideaas/ecomm/ecomm/controller/BrandController.java
+++ src/main/java/com/ideaas/ecomm/ecomm/controller/BrandController.java
@@ -1,11 +1,14 @@
 package com.ideaas.ecomm.ecomm.controller;
 
 import com.ideaas.ecomm.ecomm.domain.Brand;
+import com.ideaas.ecomm.ecomm.domain.Product;
 import com.ideaas.ecomm.ecomm.services.interfaces.IBrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,6 +37,13 @@ public class BrandController {
         return ResponseEntity.ok(brands);
     }
 
+    @GetMapping("{id}")
+    private ResponseEntity<Brand> findByID(@PathVariable final Long id) {
+        Brand brands = brandService.findById(id);
+
+        return ResponseEntity.ok(brands);
+    }
+
     @PostMapping
     private ResponseEntity<Brand> save(@RequestBody Brand brand) {
         final Brand savedBrand = brandService.save(brand);
@@ -46,6 +56,21 @@ public class BrandController {
         final Brand updatedBrand = brandService.update(brand);
 
         return ResponseEntity.ok(updatedBrand);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable final Long id){
+        final Brand brandToDelete = brandService.get(id);
+        brandService.delete(brandToDelete);
+
+        return ResponseEntity.accepted().body("Size deleted succesfully");
+    }
+
+    @GetMapping("{id}/products")
+    private ResponseEntity<List<Product>> findProductsInBrand(@PathVariable final Long id) {
+        List<Product> products = brandService.findProductsInBrand(id);
+
+        return ResponseEntity.ok(products);
     }
 
 }
--- src/main/java/com/ideaas/ecomm/ecomm/services/BrandService.java
+++ src/main/java/com/ideaas/ecomm/ecomm/services/BrandService.java
@@ -1,21 +1,28 @@
 package com.ideaas.ecomm.ecomm.services;
 
 import com.ideaas.ecomm.ecomm.domain.Brand;
+import com.ideaas.ecomm.ecomm.domain.Product;
 import com.ideaas.ecomm.ecomm.repository.BrandDao;
 import com.ideaas.ecomm.ecomm.services.interfaces.IBrandService;
+import com.ideaas.ecomm.ecomm.services.interfaces.IProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class BrandService implements IBrandService {
 
     private BrandDao dao;
+    private IProductService productService;
 
     @Autowired
-    public BrandService(final BrandDao dao) {
+    public BrandService(final BrandDao dao, final IProductService productService) {
         this.dao = dao;
+        this.productService = productService;
     }
 
     @Override
@@ -36,6 +43,39 @@ public class BrandService implements IBrandService {
     @Override
     public Brand update(Brand brand) {
         return dao.save(brand);
+    }
+
+	@Override
+	public Brand get(Long id) {
+		return dao.findById(id).get();
+	}
+
+	@Override
+	public void delete(Brand brandToDelete) {
+        List<Product> products = productService.All();
+        for (Product product: products){
+            Long brandId = brandToDelete.getId();
+            if(product.getBrand() != null && product.getBrand().getId() == brandId){
+                product.setBrand(null);
+            }
+        }
+		dao.delete(brandToDelete);
+		
+	}
+
+    @Override
+    public List<Product> findProductsInBrand(Long id) {
+        Brand brand = this.findById(id);
+        List<Product> products = productService.All();
+        List<Product> productsOfBrand = new ArrayList<>();
+        for (Product product: products){
+            Long brandId = brand.getId();
+            if(product.getBrand() != null && product.getBrand().getId() == brandId){
+                productsOfBrand.add(product);
+            }
+        }
+
+        return productsOfBrand;
     }
 
 }
