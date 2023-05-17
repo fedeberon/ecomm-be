c9368335d76097dce0ea0dcd2e19e696c385612f
Leonardo <leonardo.couar@gmail.com>
2023-05-17 10:26:31 -0300

    length 200

--- src/main/java/com/ideaas/ecomm/ecomm/domain/Product.java
+++ src/main/java/com/ideaas/ecomm/ecomm/domain/Product.java
@@ -27,7 +27,7 @@ public class Product {
     @Column(name = "PRO_NAME")
     private String name;
 
-    @Column(name = "PRO_DESCRIPTION")
+    @Column(name = "PRO_DESCRIPTION",length = 2000)
     private String description;
 
     @Column(name = "PRO_CODE")
