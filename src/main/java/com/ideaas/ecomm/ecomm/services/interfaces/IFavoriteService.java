34a6c8467b9a275dc3cfcff75810753834fbd14c
Alan Medina <medinaalangabriel@gmail.com>
2024-02-08 16:19:54 -0300

    Endpoint para verificar si favorito existe, no se puede guardar el mismo fav 2 veces.

--- src/main/java/com/ideaas/ecomm/ecomm/services/interfaces/IFavoriteService.java
+++ src/main/java/com/ideaas/ecomm/ecomm/services/interfaces/IFavoriteService.java
@@ -1,11 +1,15 @@
 package com.ideaas.ecomm.ecomm.services.interfaces;
 
 import com.ideaas.ecomm.ecomm.domain.Favorite;
+import com.ideaas.ecomm.ecomm.domain.Product;
+import com.ideaas.ecomm.ecomm.domain.User;
 import org.springframework.data.domain.Page;
 
 public interface IFavoriteService {
     Favorite save (Favorite favorite);
     Favorite get (Long id);
+
+    Long getFavoriteStatus (User user, Product product);
     void delete(Favorite favorite);
     Page<Favorite> findByUser (String username, Boolean asc, Integer page, Integer size);
 }
