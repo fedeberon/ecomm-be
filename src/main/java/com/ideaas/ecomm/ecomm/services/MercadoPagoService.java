7c825577e54341667d968a67208cd6acd08b07b4
Alan Medina <medinaalangabriel@gmail.com>
2024-02-19 18:18:20 -0300

    Ahora el sitio es capaz de recibir webhooks de MP.

--- src/main/java/com/ideaas/ecomm/ecomm/services/MercadoPagoService.java
+++ src/main/java/com/ideaas/ecomm/ecomm/services/MercadoPagoService.java
@@ -1,7 +1,12 @@
 package com.ideaas.ecomm.ecomm.services;
 
 import com.ideaas.ecomm.ecomm.domain.Checkout;
+import com.ideaas.ecomm.ecomm.domain.Product;
+import com.ideaas.ecomm.ecomm.domain.ProductToCart;
+import com.ideaas.ecomm.ecomm.enums.CheckoutState;
+import com.ideaas.ecomm.ecomm.services.interfaces.ICheckoutService;
 import com.ideaas.ecomm.ecomm.services.interfaces.IMercadoPagoService;
+import com.ideaas.ecomm.ecomm.services.interfaces.IProductService;
 import com.mercadopago.MercadoPago;
 import com.mercadopago.exceptions.MPConfException;
 import com.mercadopago.exceptions.MPException;
@@ -10,6 +15,9 @@ import com.mercadopago.resources.datastructures.preference.BackUrls;
 import com.mercadopago.resources.datastructures.preference.ExcludedPaymentMethod;
 import com.mercadopago.resources.datastructures.preference.Item;
 import com.mercadopago.resources.datastructures.preference.PaymentMethods;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,6 +29,7 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class MercadoPagoService implements IMercadoPagoService {
@@ -29,15 +38,21 @@ public class MercadoPagoService implements IMercadoPagoService {
     private String callbackurl;
     @Value("${mercadoPagoNotificationUrl}")
     private String notificationUrl;
-
     @Value("${mercadoPagoGetPayment}")
     private String getPayment;
 
+
+    private IProductService productService;
+    private ICheckoutService checkoutService;
+
     private final String ACCESS_TOKEN = "TEST-7210183612571177-101815-da1016bb668135d6ba273a4465407cca-1515196309";
 
-    public MercadoPagoService() {
+    @Autowired
+    public MercadoPagoService(IProductService productService, ICheckoutService checkoutService) {
+        this.productService = productService;
+        this.checkoutService = checkoutService;
+
         try {
-            //Token de testing
             MercadoPago.SDK.setAccessToken(ACCESS_TOKEN);
         } catch (MPConfException e) {
             e.printStackTrace();
@@ -55,9 +70,30 @@ public class MercadoPagoService implements IMercadoPagoService {
         RequestEntity<Void> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, URI.create(uri));
 
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.exchange(requestEntity, String.class).getBody();
 
-        System.out.println(result);
+        try {
+            JSONObject json = new JSONObject(restTemplate.exchange(requestEntity, String.class).getBody());
+            String status = json.getString("status");
+            Long externalReference = Long.parseLong(json.getString("external_reference"));
+
+            Checkout checkout = checkoutService.get(externalReference);
+
+            if (status.equals("approved")) {
+                checkoutService.changeStateTo(CheckoutState.PAID_OUT, externalReference);
+            } else if (isInProcessStatus(status)) {
+                checkoutService.changeStateTo(CheckoutState.IN_PROCESS, externalReference);
+            } else {
+                checkoutService.changeStateTo(CheckoutState.REJECTED, externalReference);
+            }
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isInProcessStatus(String status) {
+        return status.equals("pending") || status.equals("authorized")
+                || status.equals("in_process") || status.equals("in_mediation");
     }
 
 
@@ -65,6 +101,7 @@ public class MercadoPagoService implements IMercadoPagoService {
     public Preference createPreference(final Checkout checkout) {
         try {
             Preference preference = preparePreference(checkout);
+            ArrayList<Item> getItemsFromCheckout = getItemsFromCheckout(checkout);
 
             BackUrls backUrls = new BackUrls(
                     callbackurl,
@@ -75,7 +112,7 @@ public class MercadoPagoService implements IMercadoPagoService {
             preference.setExternalReference(String.valueOf(checkout.getId()));
             preference.setPaymentMethods(removeCash());
             preference.setNotificationUrl(notificationUrl);
-            preference.setAdditionalInfo("TEXT");
+            preference.setItems(getItemsFromCheckout);
             preference.save();
 
             return preference;
@@ -83,6 +120,27 @@ public class MercadoPagoService implements IMercadoPagoService {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private ArrayList<Item> getItemsFromCheckout(Checkout checkout){
+        ArrayList<Item> getItemsFromCheckout = new ArrayList<>();
+        List<ProductToCart> products = checkout.getProducts();
+        for (ProductToCart pro: products){
+            Product product = pro.getProduct();
+            productService.addImagesOnProduct(product);
+            Item item = new Item();
+
+            item.setCategoryId(product.getCategory().getId().toString());
+            item.setDescription(product.getDescription());
+            item.setId(product.getId().toString());
+            item.setPictureUrl(!product.getImages().isEmpty() ? product.getImages().get(0).getLink() : "");
+            item.setQuantity(pro.getQuantity());
+            item.setTitle(product.getName());
+            item.setUnitPrice(pro.getPrice().floatValue());
+
+            getItemsFromCheckout.add(item);
+        }
+        return getItemsFromCheckout;
     }
 
     private PaymentMethods removeCash (){
